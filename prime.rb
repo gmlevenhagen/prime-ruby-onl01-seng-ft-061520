89946f4f78538f6156957748479fd6142128526d
Garett Levenhagen <garettlevenhagen@gmail.com>
2020-06-24 14:30:59 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,6 +1,6 @@
-def is_prime(num)
-  (2..(num - 1)).each do |n|
-    return false if num % n == 0
+def is_prime(number)
+  (2..(number - 1)).each do |n|
+    return false if number % n == 0
   end
   true
 end
