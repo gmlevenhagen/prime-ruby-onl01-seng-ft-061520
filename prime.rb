2372fe7935cc78f81b8cf8229b41eb9221a74883
Garett Levenhagen <garettlevenhagen@gmail.com>
2020-06-24 14:35:34 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,5 +1,9 @@
-def prime(n)
+def prime?(n)
   return false if n < 2
-  (2..n/2).none?{|i| n % i == 0}
-  end
+  return true if n == 3 || n == 2
+    if (2...n-1).any?{|i| n % i == 0}
+      false
+    else
+      true
+    end
 end
